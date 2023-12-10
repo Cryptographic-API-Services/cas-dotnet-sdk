9018ed3b891209f5ba8306c174cad025e84add64
WingZer0o <mtmulch0191@outlook.com>
2023-12-09 19:51:22 -0500

    #8 rsa linux calls

--- cas-dotnet-sdk-tests/RustRsaWrapperTests.cs
+++ cas-dotnet-sdk-tests/RustRsaWrapperTests.cs
@@ -21,102 +21,54 @@ namespace CasDotnetSdkTests.Tests
         [Fact]
         public void CreateKeyPair()
         {
-            OSPlatform platform = this._operatingSystem.GetOperatingSystem();
-            if (platform == OSPlatform.Linux)
-            {
-                throw new NotImplementedException("Linux version not yet supported");
-            }
-            else
-            {
-                RsaKeyPairResult keyPair = this._RSAWrapper.GetKeyPair(4096);
-                Assert.NotNull(keyPair.PublicKey);
-                Assert.NotNull(keyPair.PrivateKey);
-            }
+            RsaKeyPairResult keyPair = this._RSAWrapper.GetKeyPair(4096);
+            Assert.NotNull(keyPair.PublicKey);
+            Assert.NotNull(keyPair.PrivateKey);
         }
 
         [Fact]
         public void RsaEncrypt()
         {
-            OSPlatform platform = this._operatingSystem.GetOperatingSystem();
-            if (platform == OSPlatform.Linux)
-            {
-                throw new NotImplementedException("Linux version not yet supported");
-            }
-            else
-            {
-                string dataToEncrypt = "EncryptingStuffIsFun";
-                string encrypted = this._RSAWrapper.RsaEncrypt(this._encryptDecryptKeyPair.PublicKey, dataToEncrypt);
-                Assert.NotEqual(dataToEncrypt, encrypted);
-            }
+            string dataToEncrypt = "EncryptingStuffIsFun";
+            string encrypted = this._RSAWrapper.RsaEncrypt(this._encryptDecryptKeyPair.PublicKey, dataToEncrypt);
+            Assert.NotEqual(dataToEncrypt, encrypted);
         }
 
         [Fact]
         public void RsaDecrypt()
         {
-            OSPlatform platform = this._operatingSystem.GetOperatingSystem();
-            if (platform == OSPlatform.Linux)
-            {
-                throw new NotImplementedException("Linux version not yet supported");
-            }
-            else
-            {
-                string dataToEncrypt = "EncryptingStuffIsFun";
-                string encrypted = this._RSAWrapper.RsaEncrypt(this._encryptDecryptKeyPair.PublicKey, dataToEncrypt);
-                string decrypted = this._RSAWrapper.RsaDecrypt(this._encryptDecryptKeyPair.PrivateKey, encrypted);
-                Assert.Equal(dataToEncrypt, decrypted);
-            }
+            string dataToEncrypt = "EncryptingStuffIsFun";
+            string encrypted = this._RSAWrapper.RsaEncrypt(this._encryptDecryptKeyPair.PublicKey, dataToEncrypt);
+            string decrypted = this._RSAWrapper.RsaDecrypt(this._encryptDecryptKeyPair.PrivateKey, encrypted);
+            Assert.Equal(dataToEncrypt, decrypted);
         }
 
         [Fact]
         public async void RsaSign()
         {
-            OSPlatform platform = this._operatingSystem.GetOperatingSystem();
-            if (platform == OSPlatform.Linux)
-            {
-                throw new NotImplementedException("Linux version not yet supported");
-            }
-            else
-            {
-                string dataToSign = "Sign This Data For Me";
-                RsaSignResult result = this._RSAWrapper.RsaSign(dataToSign, 4096);
-                Assert.NotNull(result.PublicKey);
-                Assert.NotNull(result.Signature);
-            }
+            string dataToSign = "Sign This Data For Me";
+            RsaSignResult result = this._RSAWrapper.RsaSign(dataToSign, 4096);
+            Assert.NotNull(result.PublicKey);
+            Assert.NotNull(result.Signature);
         }
 
         [Fact]
         public async void RsaVerify()
         {
-            OSPlatform platform = this._operatingSystem.GetOperatingSystem();
-            if (platform == OSPlatform.Linux)
-            {
-                throw new NotImplementedException("Linux version not yet supported");
-            }
-            else
-            {
-                string dataToSign = "Data That Needs To Be Verified";
-                RsaSignResult result = this._RSAWrapper.RsaSign(dataToSign, 4096);
-                bool isValid = this._RSAWrapper.RsaVerify(result.PublicKey, dataToSign, result.Signature);
-                Assert.Equal(true, isValid);
-            }
+            string dataToSign = "Data That Needs To Be Verified";
+            RsaSignResult result = this._RSAWrapper.RsaSign(dataToSign, 4096);
+            bool isValid = this._RSAWrapper.RsaVerify(result.PublicKey, dataToSign, result.Signature);
+            Assert.Equal(true, isValid);
         }
 
         [Fact]
         public async void RsaSignWithKey()
         {
-            OSPlatform platform = this._operatingSystem.GetOperatingSystem();
-            if (platform == OSPlatform.Linux)
-            {
-                throw new NotImplementedException("Linux version not yet supported");
-            }
-            else
-            {
-                string dataToSign = "This data needs to be signed now";
-                RsaKeyPairResult keyPair = this._RSAWrapper.GetKeyPair(2048);
-                string signature = this._RSAWrapper.RsaSignWithKey(keyPair.PrivateKey, dataToSign);
-                Assert.NotNull(signature);
-                Assert.NotEqual(dataToSign, signature);
-            }
+            string dataToSign = "This data needs to be signed now";
+            RsaKeyPairResult keyPair = this._RSAWrapper.GetKeyPair(2048);
+            string signature = this._RSAWrapper.RsaSignWithKey(keyPair.PrivateKey, dataToSign);
+            Assert.NotNull(signature);
+            Assert.NotEqual(dataToSign, signature);
         }
     }
 }
