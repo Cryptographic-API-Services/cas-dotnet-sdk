8ab50051e3ba21715fb9026194c577f451d6fd35
Mike Mulchrone <mtmulch0191@outlook.com>
2023-12-28 22:12:04 -0500

    updating get automatic refresh token

--- cas-dotnet-sdk/CASConfiguration.cs
+++ cas-dotnet-sdk/CASConfiguration.cs
@@ -1,5 +1,10 @@
-﻿using CASHelpers;
+﻿using CasDotnetSdk.Types.ApiRequests;
+using CASHelpers;
+using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CasDotnetSdk
@@ -22,6 +27,13 @@ namespace CasDotnetSdk
             set { _Token = value; }
         }
 
+        private static DateTime _TokenExpiresIn;
+        public static DateTime TokenExpiresIn
+        {
+            get { return _TokenExpiresIn; }
+            set { _TokenExpiresIn = value; }
+        }
+
         private static string _Url;
 
         public static string Url
@@ -30,9 +42,13 @@ namespace CasDotnetSdk
             set { _Url = value; }
         }
 
+        private static Timer Timer { get; set; }
+        private static int TimerInternval { get; set; }
+
         static CASConfiguration()
         {
             Url = "https://localhost:7189";
+            TimerInternval = 15;
         }
 
         public static async Task<string> GetTokenAfterApiKeySet(string apiKey)
@@ -45,9 +61,42 @@ namespace CasDotnetSdk
             {
                 response = await httpClient.GetAsync(url);
             }
-
-
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            GetTokenResponse tokenResponse = JsonSerializer.Deserialize<GetTokenResponse>(await response.Content.ReadAsStringAsync(), options);
+            Token = tokenResponse.Token;
+            // Set timer for auto token refresh
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jsonToken = tokenHandler.ReadToken(Token) as JwtSecurityToken;
+            TokenExpiresIn = jsonToken.ValidTo;
+            Timer = new Timer(GetRefreshToken, null, TimeSpan.FromSeconds(TimerInternval), TimeSpan.FromSeconds(TimerInternval));
             return apiKey;
+        }
+
+        public static async void GetRefreshToken(object state)
+        {
+            if (DateTime.UtcNow.AddMinutes(5) >= TokenExpiresIn)
+            {
+                HttpClientSingleton httpClient = HttpClientSingleton.Instance;
+                string url = Url + Constants.ApiRoutes.Token + Constants.ApiRoutes.RefreshToken;
+                httpClient.DefaultRequestHeaders.Add(Constants.HeaderNames.Authorization, Token);
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                while (!response.IsSuccessStatusCode)
+                {
+                    response = await httpClient.GetAsync(url);
+                }
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                GetTokenResponse tokenResponse = JsonSerializer.Deserialize<GetTokenResponse>(await response.Content.ReadAsStringAsync(), options);
+                Token = tokenResponse.Token;
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                JwtSecurityToken jsonToken = tokenHandler.ReadToken(Token) as JwtSecurityToken;
+                TokenExpiresIn = jsonToken.ValidTo;
+            }
         }
     }
 }
