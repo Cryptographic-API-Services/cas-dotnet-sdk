369c17a41379d067662795c1464c2904d8427154
Mike Mulchrone <mtmulch0191@outlook.com>
2023-12-29 10:21:19 -0500

    updating token cache

--- cas-dotnet-sdk/CASConfiguration.cs
+++ cas-dotnet-sdk/CASConfiguration.cs
@@ -1,5 +1,9 @@
-﻿using CASHelpers;
+﻿using CasDotnetSdk.Storage;
+using CASHelpers;
+using CASHelpers.Types.HttpResponses.UserAuthentication;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CasDotnetSdk
@@ -11,43 +15,29 @@ namespace CasDotnetSdk
         public static string ApiKey
         {
             get { return _ApiKey; }
-            set => _ApiKey = GetTokenAfterApiKeySet(value).GetAwaiter().GetResult();
-        }
-
-        private static string _Token;
-
-        public static string Token
-        {
-            get { return _Token; }
-            set { _Token = value; }
+            set => _ApiKey = TokenCache.GetTokenAfterApiKeySet(value).GetAwaiter().GetResult();
         }
 
         private static string _Url;
 
-        public static string Url
+        internal static string Url
         {
             get { return _Url; }
             set { _Url = value; }
         }
 
+        private static TokenCache _TokenCache;
+
+        internal static TokenCache TokenCache
+        {
+            get { return _TokenCache; }
+            set { _TokenCache = value; }
+        }
+
         static CASConfiguration()
         {
             Url = "https://localhost:7189";
-        }
-
-        public static async Task<string> GetTokenAfterApiKeySet(string apiKey)
-        {
-            HttpClientSingleton httpClient = HttpClientSingleton.Instance;
-            string url = Url + Constants.ApiRoutes.Token;
-            httpClient.DefaultRequestHeaders.Add(Constants.HeaderNames.ApiKey, apiKey);
-            HttpResponseMessage response = await httpClient.GetAsync(url);
-            while (!response.IsSuccessStatusCode)
-            {
-                response = await httpClient.GetAsync(url);
-            }
-
-
-            return apiKey;
+            TokenCache = new TokenCache();
         }
     }
 }
---
+++ cas-dotnet-sdk/Storage/TokenCache.cs
@@ -0,0 +1,72 @@
+﻿
+
+using CASHelpers;
+using CASHelpers.Types.HttpResponses.UserAuthentication;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CasDotnetSdk.Storage
+{
+    internal class TokenCache
+    {
+        internal string Token { get; set; }
+        private DateTime TokenExpiresIn { get; set; }
+        private Timer Timer { get; set; }
+        private int TimerInterval { get; set; }
+
+        public TokenCache()
+        {
+            this.TimerInterval = 15;
+        }
+
+        public async Task<string> GetTokenAfterApiKeySet(string apiKey)
+        {
+            HttpClientSingleton httpClient = HttpClientSingleton.Instance;
+            string url = CASConfiguration.Url + Constants.ApiRoutes.Token;
+            httpClient.DefaultRequestHeaders.Add(Constants.HeaderNames.ApiKey, apiKey);
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            while (!response.IsSuccessStatusCode)
+            {
+                response = await httpClient.GetAsync(url);
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+            GetTokenResponse tokenResponse = JsonSerializer.Deserialize<GetTokenResponse>(await response.Content.ReadAsStringAsync(), options);
+            this.Token = tokenResponse.Token;
+            this.Timer = new Timer(GetRefreshToken, null, TimeSpan.FromSeconds(this.TimerInterval), TimeSpan.FromSeconds(this.TimerInterval));
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken parsedToken = tokenHandler.ReadJwtToken(this.Token);
+            this.TokenExpiresIn = parsedToken.ValidTo;
+            return apiKey;
+        }
+
+        private async void GetRefreshToken(object state)
+        {
+            if (DateTime.UtcNow.AddMinutes(5) >= this.TokenExpiresIn)
+            {
+                HttpClientSingleton httpClient = HttpClientSingleton.Instance;
+                string url = CASConfiguration.Url + Constants.ApiRoutes.Token + Constants.ApiRoutes.RefreshToken;
+                httpClient.DefaultRequestHeaders.Add(Constants.HeaderNames.Authorization, this.Token);
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    JsonSerializerOptions options = new JsonSerializerOptions()
+                    {
+                        PropertyNameCaseInsensitive = true,
+                    };
+                    GetTokenResponse tokenResponse = JsonSerializer.Deserialize<GetTokenResponse>(await response.Content.ReadAsStringAsync(), options);
+                    this.Token = tokenResponse.Token;
+                    JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                    JwtSecurityToken parsedToken = tokenHandler.ReadJwtToken(this.Token);
+                    this.TokenExpiresIn = parsedToken.ValidTo;
+                }
+            }
+        }
+    }
+}
