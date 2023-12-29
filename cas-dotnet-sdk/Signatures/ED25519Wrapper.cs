49ae19b207d90dd6085ee98bf7cdccbfd02f4489
Mike Mulchrone <mtmulch0191@outlook.com>
2023-12-29 02:16:42 -0500

    #38 ed25519 remove string api

--- cas-dotnet-sdk/Signatures/ED25519Wrapper.cs
+++ cas-dotnet-sdk/Signatures/ED25519Wrapper.cs
@@ -47,24 +47,6 @@ namespace CasDotnetSdk.Signatures
             public int public_key_length { get; set; }
         }
 
-        public string GetKeyPair()
-        {
-            if (this._platform == OSPlatform.Linux)
-            {
-                IntPtr keyPairPtr = ED25519LinuxWrapper.get_ed25519_key_pair();
-                string keyPair = Marshal.PtrToStringAnsi(keyPairPtr);
-                ED25519LinuxWrapper.free_cstring(keyPairPtr);
-                return keyPair;
-            }
-            else
-            {
-                IntPtr keyPairPtr = ED25519WindowsWrapper.get_ed25519_key_pair();
-                string keyPair = Marshal.PtrToStringAnsi(keyPairPtr);
-                ED25519WindowsWrapper.free_cstring(keyPairPtr);
-                return keyPair;
-            }
-        }
-
         public byte[] GetKeyPairBytes()
         {
             if (this._platform == OSPlatform.Linux)
@@ -82,43 +64,6 @@ namespace CasDotnetSdk.Signatures
                 Marshal.Copy(resultStruct.key_pair, keyPairResult, 0, resultStruct.length);
                 ED25519WindowsWrapper.free_bytes(resultStruct.key_pair);
                 return keyPairResult;
-            }
-        }
-
-        public Ed25519SignatureResult Sign(string keyBytes, string dataToSign)
-        {
-            if (string.IsNullOrEmpty(keyBytes))
-            {
-                throw new Exception("You need pass in the key bytes to sign data");
-            }
-            if (string.IsNullOrEmpty(dataToSign))
-            {
-                throw new Exception("You need to pass in data to sign, to sign data");
-            }
-
-            if (this._platform == OSPlatform.Linux)
-            {
-                Ed25519SignatureStruct signatureStruct = ED25519LinuxWrapper.sign_with_key_pair(keyBytes, dataToSign);
-                Ed25519SignatureResult result = new Ed25519SignatureResult()
-                {
-                    Signature = Marshal.PtrToStringAnsi(signatureStruct.Signature),
-                    PublicKey = Marshal.PtrToStringAnsi(signatureStruct.Public_Key)
-                };
-                ED25519LinuxWrapper.free_cstring(signatureStruct.Signature);
-                ED25519LinuxWrapper.free_cstring(signatureStruct.Public_Key);
-                return result;
-            }
-            else
-            {
-                Ed25519SignatureStruct signatureStruct = ED25519WindowsWrapper.sign_with_key_pair(keyBytes, dataToSign);
-                Ed25519SignatureResult result = new Ed25519SignatureResult()
-                {
-                    Signature = Marshal.PtrToStringAnsi(signatureStruct.Signature),
-                    PublicKey = Marshal.PtrToStringAnsi(signatureStruct.Public_Key)
-                };
-                ED25519WindowsWrapper.free_cstring(signatureStruct.Signature);
-                ED25519WindowsWrapper.free_cstring(signatureStruct.Public_Key);
-                return result;
             }
         }
 
@@ -165,31 +110,6 @@ namespace CasDotnetSdk.Signatures
             }
         }
 
-        public bool Verify(string keyBytes, string signature, string dataToVerify)
-        {
-            if (string.IsNullOrEmpty(keyBytes))
-            {
-                throw new Exception("You need pass in the key bytes to verify data");
-            }
-            if (string.IsNullOrEmpty(signature))
-            {
-                throw new Exception("You need to pass in the signature to verify data");
-            }
-            if (string.IsNullOrEmpty(dataToVerify))
-            {
-                throw new Exception("You need to pass in data to verify, to verify data");
-            }
-
-            if (this._platform == OSPlatform.Linux)
-            {
-                return ED25519LinuxWrapper.verify_with_key_pair(keyBytes, signature, dataToVerify);
-            }
-            else
-            {
-                return ED25519WindowsWrapper.verify_with_key_pair(keyBytes, signature, dataToVerify);
-            }
-        }
-
         public bool VerifyBytes(byte[] keyPair, byte[] signature, byte[] dataToVerify)
         {
             if (keyPair?.Length == 0)
@@ -212,31 +132,6 @@ namespace CasDotnetSdk.Signatures
             else
             {
                 return ED25519WindowsWrapper.verify_with_key_pair_bytes(keyPair, keyPair.Length, signature, signature.Length, dataToVerify, dataToVerify.Length);
-            }
-        }
-
-        public bool VerifyWithPublicKey(string publicKey, string signature, string dataToVerify)
-        {
-            if (string.IsNullOrEmpty(publicKey))
-            {
-                throw new Exception("You need pass in the key bytes to verify data");
-            }
-            if (string.IsNullOrEmpty(signature))
-            {
-                throw new Exception("You need to pass in the signature to verify data");
-            }
-            if (string.IsNullOrEmpty(dataToVerify))
-            {
-                throw new Exception("You need to pass in data to verify, to verify data");
-            }
-
-            if (this._platform == OSPlatform.Linux)
-            {
-                return ED25519LinuxWrapper.verify_with_public_key(publicKey, signature, dataToVerify);
-            }
-            else
-            {
-                return ED25519WindowsWrapper.verify_with_public_key(publicKey, signature, dataToVerify);
             }
         }
 
--- cas-dotnet-sdk/Signatures/Windows/ED25519WindowsWrapper.cs
+++ cas-dotnet-sdk/Signatures/Windows/ED25519WindowsWrapper.cs
@@ -6,29 +6,16 @@ namespace CasDotnetSdk.Signatures.Windows
 {
     internal static class ED25519WindowsWrapper
     {
-        [DllImport("cas_core_lib.dll")]
-        public static extern IntPtr get_ed25519_key_pair();
 
         [DllImport("cas_core_lib.dll")]
         public static extern Ed25519KeyPairBytesResultStruct get_ed25519_key_pair_bytes();
-
-        [DllImport("cas_core_lib.dll")]
-        public static extern Ed25519SignatureStruct sign_with_key_pair(string keyBytes, string dataToSign);
 
         [DllImport("cas_core_lib.dll")]
         public static extern Ed25519ByteSignatureResultStruct sign_with_key_pair_bytes(byte[] keyPair, int keyPairLength, byte[] message, int messageLength);
 
         [DllImport("cas_core_lib.dll")]
         [return: MarshalAs(UnmanagedType.I1)]
-        public static extern bool verify_with_key_pair(string keyBytes, string signature, string dataToVerify);
-
-        [DllImport("cas_core_lib.dll")]
-        [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool verify_with_key_pair_bytes(byte[] keyPair, int keyPairLength, byte[] signature, int signatureLength, byte[] message, int messageLength);
-
-        [DllImport("cas_core_lib.dll")]
-        [return: MarshalAs(UnmanagedType.I1)]
-        public static extern bool verify_with_public_key(string publicKey, string signature, string dataToVerify);
 
         [DllImport("cas_core_lib.dll")]
         [return: MarshalAs(UnmanagedType.I1)]
