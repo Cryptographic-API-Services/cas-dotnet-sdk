afbc799883c91dd216a31e5876605626f08e1cba
Mike Mulchrone <mikemulchrone987@gmail.com>
2024-01-14 17:39:38 -0500

    Hybrid Encryption

---
+++ cas-dotnet-sdk/Hybrid/HybridEncryptionWrapper.cs
@@ -0,0 +1,61 @@
+﻿using CasDotnetSdk.Asymmetric;
+using CasDotnetSdk.Http;
+using CasDotnetSdk.Hybrid.Types;
+using CasDotnetSdk.Symmetric;
+using CasDotnetSdk.Symmetric.Types;
+using CASHelpers.Types.HttpResponses.BenchmarkAPI;
+using System;
+using System.Reflection;
+
+namespace CasDotnetSdk.Hybrid
+{
+    /// <summary>
+    /// In CAS, Hybrid encryption is a combination of RSA Encryption for the AES-GCM key.
+    /// The user is expected to generate an RSA key pair and AES Key and Nonce before hand if they want to utilize using the same key for multiple data sets
+    /// Eventually we may want to move this logic directly into cas-core-lib.
+    /// </summary>
+    public class HybridEncryptionWrapper
+    {
+        private readonly BenchmarkSender _benchmarkSender;
+        private readonly AESWrapper _aesWrapper;
+        private readonly RSAWrapper _rsaWrapper;
+
+        public HybridEncryptionWrapper()
+        {
+            this._benchmarkSender = new BenchmarkSender();
+            this._aesWrapper = new AESWrapper();
+            this._rsaWrapper = new RSAWrapper();
+        }
+
+        public AESRSAHybridEncryptResult EncryptAESRSAHybrid(byte[] dataToEncrypt, AESRSAHybridInitializer initilizer)
+        {
+            DateTime start = DateTime.UtcNow;
+            byte[] aesEncryptResult = (initilizer.AesType == 128)
+                ? this._aesWrapper.Aes128BytesEncrypt(initilizer.AesNonce, initilizer.AesKey, dataToEncrypt)
+                : this._aesWrapper.Aes256EncryptBytes(initilizer.AesNonce, initilizer.AesKey, dataToEncrypt);
+            byte[] encryptedAesKey = this._rsaWrapper.RsaEncryptBytes(initilizer.RsaKeyPair.PublicKey, Convert.FromBase64String(initilizer.AesKey));
+            AESRSAHybridEncryptResult result = new AESRSAHybridEncryptResult()
+            {
+                CipherText = aesEncryptResult,
+                EncryptedAesKey = encryptedAesKey,
+                AesType = initilizer.AesType,
+                AesNonce = initilizer.AesNonce,
+            };
+            DateTime end = DateTime.UtcNow;
+            this._benchmarkSender.SendNewBenchmarkMethod(MethodBase.GetCurrentMethod().Name, start, end, BenchmarkMethodType.Hash, nameof(HybridEncryptionWrapper));
+            return result;
+        }
+
+        public byte[] DecryptAESRSAHybrid(string rsaPrivateKey, AESRSAHybridEncryptResult encryptResult)
+        {
+            DateTime start = DateTime.UtcNow;
+            byte[] plaintextAesKey = this._rsaWrapper.RsaDecryptBytes(rsaPrivateKey, encryptResult.EncryptedAesKey);
+            byte[] plaintext = (encryptResult.AesType == 128)
+                ? this._aesWrapper.Aes128BytesDecrypt(encryptResult.AesNonce, Convert.ToBase64String(plaintextAesKey), encryptResult.CipherText)
+                : this._aesWrapper.Aes256DecryptBytes(encryptResult.AesNonce, Convert.ToBase64String(plaintextAesKey), encryptResult.CipherText);
+            DateTime end = DateTime.UtcNow;
+            this._benchmarkSender.SendNewBenchmarkMethod(MethodBase.GetCurrentMethod().Name, start, end, BenchmarkMethodType.Hash, nameof(HybridEncryptionWrapper));
+            return plaintext;
+        }
+    }
+}
---
+++ cas-dotnet-sdk/Hybrid/Types/AESRSAHybridEncryptResult.cs
@@ -0,0 +1,12 @@
+﻿using CasDotnetSdk.Asymmetric.Types;
+
+namespace CasDotnetSdk.Hybrid.Types
+{
+    public class AESRSAHybridEncryptResult
+    { 
+        public int AesType { get; set; }
+        public string AesNonce { get; set; }
+        public byte[] CipherText { get; set; }
+        public byte[] EncryptedAesKey { get; set; }
+    }
+}
