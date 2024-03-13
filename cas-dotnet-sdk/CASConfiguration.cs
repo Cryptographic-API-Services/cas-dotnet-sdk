eb0512a6e1b5bd3ca94f25d9da579e7268d3b52f
WingZer0o <mtmulch0191@outlook.com>
2024-03-12 21:34:12 -0400

    adding some comments to the SDK

--- cas-dotnet-sdk/CASConfiguration.cs
+++ cas-dotnet-sdk/CASConfiguration.cs
@@ -23,6 +23,10 @@ namespace CasDotnetSdk
         }
 
         private static string _ApiKey;
+
+        /// <summary>
+        /// This is the property where you set your CAS User account API key from the CAS Dashboard.
+        /// </summary>
         public static string ApiKey
         {
             get { return _ApiKey; }
@@ -36,6 +40,10 @@ namespace CasDotnetSdk
         }
 
         private static bool _IsDevelopment;
+
+        /// <summary>
+        /// This method is mostly for development purposes of the SDk. We don't recommend changing this in a production environment.
+        /// </summary>
         public static bool IsDevelopment
         {
             get { return _IsDevelopment; }
