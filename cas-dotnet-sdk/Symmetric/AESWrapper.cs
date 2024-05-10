829af05c599bdc3533f9fd4b798e548dd83ddacc
WingZer0o <mtmulch0191@outlook.com>
2024-05-10 19:42:27 -0400

    changing names

--- cas-dotnet-sdk/Symmetric/AESWrapper.cs
+++ cas-dotnet-sdk/Symmetric/AESWrapper.cs
@@ -152,7 +152,7 @@ namespace CasDotnetSdk.Symmetric
             }
         }
 
-        public byte[] Aes256EncryptBytes(string nonceKey, string key, byte[] toEncrypt, bool sendBenchmark = true)
+        public byte[] Aes256Encrypt(string nonceKey, string key, byte[] toEncrypt, bool sendBenchmark = true)
         {
             if (string.IsNullOrEmpty(nonceKey))
             {
@@ -192,7 +192,7 @@ namespace CasDotnetSdk.Symmetric
             }
         }
 
-        public byte[] Aes256DecryptBytes(string nonceKey, string key, byte[] toDecrypt)
+        public byte[] Aes256Decrypt(string nonceKey, string key, byte[] toDecrypt)
         {
             if (string.IsNullOrEmpty(nonceKey))
             {
@@ -230,7 +230,7 @@ namespace CasDotnetSdk.Symmetric
             }
         }
 
-        public byte[] Aes128BytesEncrypt(string nonceKey, string key, byte[] dataToEncrypt)
+        public byte[] Aes128Encrypt(string nonceKey, string key, byte[] dataToEncrypt)
         {
             if (string.IsNullOrEmpty(nonceKey))
             {
@@ -268,7 +268,7 @@ namespace CasDotnetSdk.Symmetric
             }
         }
 
-        public byte[] Aes128BytesDecrypt(string nonceKey, string key, byte[] dataToDecrypt)
+        public byte[] Aes128Decrypt(string nonceKey, string key, byte[] dataToDecrypt)
         {
             if (string.IsNullOrEmpty(nonceKey))
             {
