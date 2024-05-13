4b6439ef24236e33db5a2c1caf67c9333bc3812f
WingZer0o <mtmulch0191@outlook.com>
2024-05-13 18:18:48 -0400

    #97 removing argon2 hashing multiple passwords

--- cas-dotnet-sdk/PasswordHashers/Windows/Argon2WindowsWrapper.cs
+++ cas-dotnet-sdk/PasswordHashers/Windows/Argon2WindowsWrapper.cs
@@ -14,19 +14,12 @@ namespace CasDotnetSdk.PasswordHashers.Windows
         public static extern IntPtr argon2_hash_threadpool(string passToHash);
 
         [DllImport("\\Contents\\cas_core_lib.dll")]
-        public static extern Argon2ThreadResult argon2_hash_thread(string[] passwordsToHash, int numOfPasswords);
-
-        [DllImport("\\Contents\\cas_core_lib.dll")]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool argon2_verify(string hashedPassword, string passToVerify);
 
         [DllImport("\\Contents\\cas_core_lib.dll")]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool argon2_verify_threadpool(string hashedPassword, string passToVerify);
-
-        [DllImport("\\Contents\\cas_core_lib.dll")]
-        [return: MarshalAs(UnmanagedType.I1)]
-        public static extern bool argon2_verify_thread(string hashedPassword, string passToVerify);
 
         [DllImport("\\Contents\\cas_core_lib.dll")]
         public static extern void free_cstring(IntPtr stringToFree);
