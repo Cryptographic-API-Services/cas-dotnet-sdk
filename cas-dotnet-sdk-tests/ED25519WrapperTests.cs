6f348850074000f06e743566f47157cd68dccae6
Mike Mulchrone <mtmulch0191@outlook.com>
2024-05-26 13:49:36 -0400

    #103 ed25519 threadpool implemenation and test cases

--- cas-dotnet-sdk-tests/ED25519WrapperTests.cs
+++ cas-dotnet-sdk-tests/ED25519WrapperTests.cs
@@ -22,11 +22,31 @@ namespace CasDotnetSdkTests.Tests
         }
 
         [Fact]
+        public void GetKeyPairBytesThreadpool()
+        {
+            byte[] keyPair = this._wrapper.GetKeyPairBytesThreadpool();
+            Assert.NotNull(keyPair);
+            Assert.NotEmpty(keyPair);
+        }
+
+        [Fact]
         public void SignDataByes()
         {
             byte[] keyPair = this._wrapper.GetKeyPairBytes();
             byte[] dataToSign = Encoding.UTF8.GetBytes("SignThisDataWithEd25519Dalek");
             Ed25519ByteSignatureResult result = this._wrapper.SignBytes(keyPair, dataToSign);
+            Assert.NotNull(result.Signature);
+            Assert.NotNull(result.PublicKey);
+            Assert.NotEmpty(result.Signature);
+            Assert.NotEmpty(result.PublicKey);
+        }
+
+        [Fact]
+        public void SignDataByesThreadpool()
+        {
+            byte[] keyPair = this._wrapper.GetKeyPairBytes();
+            byte[] dataToSign = Encoding.UTF8.GetBytes("SignThisDataWithEd25519Dalek");
+            Ed25519ByteSignatureResult result = this._wrapper.SignBytesThreadpool(keyPair, dataToSign);
             Assert.NotNull(result.Signature);
             Assert.NotNull(result.PublicKey);
             Assert.NotEmpty(result.Signature);
@@ -44,12 +64,31 @@ namespace CasDotnetSdkTests.Tests
         }
 
         [Fact]
+        public void VerifyBytesThreadpool()
+        {
+            byte[] keyPair = this._wrapper.GetKeyPairBytesThreadpool();
+            byte[] dataToSign = Encoding.UTF8.GetBytes("ThisIsGarbageDataThatShouldBeIncreased");
+            Ed25519ByteSignatureResult signatureResult = this._wrapper.SignBytesThreadpool(keyPair, dataToSign);
+            bool isValid = this._wrapper.VerifyBytes(keyPair, signatureResult.Signature, dataToSign);
+            Assert.True(isValid);
+        }
+
+        [Fact]
         public void VerifyWithPublicKeyBytes()
         {
             byte[] keyPair = this._wrapper.GetKeyPairBytes();
             byte[] dataToSign = Encoding.UTF8.GetBytes("ThisIsBadDataToVerifyWithEd25519-Dalek");
             Ed25519ByteSignatureResult result = this._wrapper.SignBytes(keyPair, dataToSign);
             bool isValid = this._wrapper.VerifyWithPublicKeyBytes(result.PublicKey, result.Signature, dataToSign);
+        }
+
+        [Fact]
+        public void VerifyWithPublicKeyBytesThreadpool()
+        {
+            byte[] keyPair = this._wrapper.GetKeyPairBytesThreadpool();
+            byte[] dataToSign = Encoding.UTF8.GetBytes("ThisIsBadDataToVerifyWithEd25519-Dalek");
+            Ed25519ByteSignatureResult result = this._wrapper.SignBytesThreadpool(keyPair, dataToSign);
+            bool isValid = this._wrapper.VerifyWithPublicKeyBytesThreadpool(result.PublicKey, result.Signature, dataToSign);
         }
     }
 }
