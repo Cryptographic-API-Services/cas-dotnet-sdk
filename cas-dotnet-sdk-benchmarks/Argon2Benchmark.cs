58689c334e5780453ea740f8d76fcf198ae4e902
Mike Mulchrone <mtmulch0191@outlook.com>
2024-06-01 22:13:27 -0400

    #22 beginning of benchmark

---
+++ cas-dotnet-sdk-benchmarks/Argon2Benchmark.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using CasDotnetSdk.PasswordHashers;
+
+namespace CasDotnetSdkBenchmarks
+{
+    public class Argon2Benchmark
+    {
+        private readonly Argon2Wrapper _argon2Wrapper;
+        private readonly string _passwordToHash;
+        public Argon2Benchmark()
+        {
+            this._argon2Wrapper = new Argon2Wrapper();
+            this._passwordToHash = "asdasdasd23454674567fdgh34tdcfb2345";
+        }
+
+        [Benchmark]
+        public string HashPassword()
+        {
+            return this._argon2Wrapper.HashPassword(this._passwordToHash);
+        }
+
+        [Benchmark]
+        public bool Verify()
+        {
+            string hash = this._argon2Wrapper.HashPassword(this._passwordToHash);
+            return this._argon2Wrapper.Verify(hash, this._passwordToHash);
+        }
+    }
+}
---
+++ cas-dotnet-sdk-benchmarks/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using CasDotnetSdkBenchmarks;
+
+BenchmarkRunner.Run<Argon2Benchmark>();
