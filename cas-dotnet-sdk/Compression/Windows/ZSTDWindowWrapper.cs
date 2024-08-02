d2ae464e77aee02223486988184ab73e010e3d56
Mike Mulchrone <mtmulch0191@outlook.com>
2024-08-01 21:24:48 -0400

    #117 zstd compression starting point moved over dll and so and created wrapper classes

---
+++ cas-dotnet-sdk/Compression/Windows/ZSTDWindowWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasDotnetSdk.Compression.Windows
+{
+    internal static class ZSTDWindowWrapper
+    {
+    }
+}
