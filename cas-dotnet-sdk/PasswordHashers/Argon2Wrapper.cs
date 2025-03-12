3efaa10bc1281b99574a0a38ebcab64d21abd70a
Mike Mulchrone <mikemulchrone987@gmail.com>
2025-03-11 20:24:58 -0400

    argon2 key deriviation for aes

--- cas-dotnet-sdk/PasswordHashers/Argon2Wrapper.cs
+++ cas-dotnet-sdk/PasswordHashers/Argon2Wrapper.cs
@@ -1,5 +1,6 @@
 ﻿using CasDotnetSdk.Helpers;
 using CasDotnetSdk.PasswordHashers.Linux;
+using CasDotnetSdk.PasswordHashers.Types;
 using CasDotnetSdk.PasswordHashers.Windows;
 using CASHelpers.Types.HttpResponses.BenchmarkAPI;
 using System;
@@ -151,6 +152,66 @@ namespace CasDotnetSdk.PasswordHashers
             else
             {
                 bool result = Argon2WindowsWrapper.argon2_verify_threadpool(hashedPassword, verifyPassword);
+                DateTime end = DateTime.UtcNow;
+                this._sender.SendNewBenchmarkMethod(MethodBase.GetCurrentMethod().Name, start, end, BenchmarkMethodType.PasswordHash, nameof(Argon2Wrapper));
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Derives an 32-byte AES256 key based off the password passed in using Argon2.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public byte[] DeriveAES256Key(string password)
+        {
+            DateTime start = DateTime.UtcNow;
+            if (this._platform == OSPlatform.Linux)
+            {
+                Argon2KDFResult kdfResult = Argon2LinuxWrapper.argon2_derive_aes_256_key(password);
+                byte[] result = new byte[kdfResult.length];
+                Marshal.Copy(kdfResult.key, result, 0, kdfResult.length);
+                FreeMemoryHelper.FreeBytesMemory(kdfResult.key);
+                DateTime end = DateTime.UtcNow;
+                this._sender.SendNewBenchmarkMethod(MethodBase.GetCurrentMethod().Name, start, end, BenchmarkMethodType.PasswordHash, nameof(Argon2Wrapper));
+                return result;
+            }
+            else
+            {
+                Argon2KDFResult kdfResult = Argon2WindowsWrapper.argon2_derive_aes_256_key(password);
+                byte[] result = new byte[kdfResult.length];
+                Marshal.Copy(kdfResult.key, result, 0, kdfResult.length);
+                FreeMemoryHelper.FreeBytesMemory(kdfResult.key);
+                DateTime end = DateTime.UtcNow;
+                this._sender.SendNewBenchmarkMethod(MethodBase.GetCurrentMethod().Name, start, end, BenchmarkMethodType.PasswordHash, nameof(Argon2Wrapper));
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Derives an 16-byte AES128 key based off the password passed in using Argon2.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public byte[] DeriveAES128Key(string password)
+        {
+            DateTime start = DateTime.UtcNow;
+            if (this._platform == OSPlatform.Linux)
+            {
+                Argon2KDFResult kdfResult = Argon2LinuxWrapper.argon2_derive_aes_128_key(password);
+                byte[] result = new byte[kdfResult.length];
+                Marshal.Copy(kdfResult.key, result, 0, kdfResult.length);
+                FreeMemoryHelper.FreeBytesMemory(kdfResult.key);
+                DateTime end = DateTime.UtcNow;
+                this._sender.SendNewBenchmarkMethod(MethodBase.GetCurrentMethod().Name, start, end, BenchmarkMethodType.PasswordHash, nameof(Argon2Wrapper));
+                return result;
+            }
+            else
+            {
+                Argon2KDFResult kdfResult = Argon2WindowsWrapper.argon2_derive_aes_128_key(password);
+                byte[] result = new byte[kdfResult.length];
+                Marshal.Copy(kdfResult.key, result, 0, kdfResult.length);
+                FreeMemoryHelper.FreeBytesMemory(kdfResult.key);
                 DateTime end = DateTime.UtcNow;
                 this._sender.SendNewBenchmarkMethod(MethodBase.GetCurrentMethod().Name, start, end, BenchmarkMethodType.PasswordHash, nameof(Argon2Wrapper));
                 return result;
---
+++ cas-dotnet-sdk/PasswordHashers/Types/Argon2KDFResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CasDotnetSdk.PasswordHashers.Types
+{
+    internal struct Argon2KDFResult
+    {
+        public IntPtr key { get; set; }
+        public int length { get; set; }
+    }
+}
