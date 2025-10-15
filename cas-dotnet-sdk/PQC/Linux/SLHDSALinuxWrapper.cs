1f18aade6e744c52b0c6bc369ea7eb616edd8f81
WingZer0o <mikemulchrone987@gmail.com>
2025-10-14 23:56:06 -0400

    adding base classes and stuff

---
+++ cas-dotnet-sdk/PQC/Linux/SLHDSALinuxWrapper.cs
@@ -0,0 +1,11 @@
+﻿using CasDotnetSdk.PQC.Types;
+using System.Runtime.InteropServices;
+
+namespace CasDotnetSdk.PQC.Linux
+{
+    public class SLHDSALinuxWrapper
+    {
+        [DllImport("libcas_core_lib.so")]
+        public static extern SLHDSAKeyPairStruct slh_dsa_generate_signing_and_verification_key();
+    }
+}
---
+++ cas-dotnet-sdk/PQC/Windows/SLHDSALinuxWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasDotnetSdk.PQC.Windows
+{
+    public class SLHDSALinuxWrapper
+    {
+    }
+}
