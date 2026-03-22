8b7c3f4dc099870ee2cdb6f945a0a760e1bd22fc
Mike Mulchrone <mikemulchrone987@gmail.com>
2026-03-22 18:31:15 -0400

    remove digital signature

--- cas-dotnet-sdk/DigitalSignature/Types/SHAED25519DalekDigitialSignatureResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CasDotnetSdk.DigitalSignature.Types
-{
-    public class SHAED25519DalekDigitialSignatureResult
-    {
-        public byte[] PublicKey { get; set; }
-        public byte[] Signature { get; set; }
-    }
-}
--- cas-dotnet-sdk/DigitalSignature/Types/SHARSADigitalSignatureResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CasDotnetSdk.DigitalSignature.Types
-{
-    public class SHARSADigitalSignatureResult
-    {
-        public string PublicKey { get; set; }
-        public string PrivateKey { get; set; }
-        public byte[] Signature { get; set; }
-    }
-}
